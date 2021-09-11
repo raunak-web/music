5c56aab43ab6f6b183aa23758a638774c08b433e
raunak-web <65885395+raunak-web@users.noreply.github.com>
2021-09-11 23:45:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+
+  const p1 = document.getElementById('play')
+  const s1 = document.getElementById("s2")
+  var count = 0;
+ function fuc1(){  
+   if (count == 0) {
+    count = 1;
+    s1.play();
+    console.log("run");
+  } else {
+    count = 0;
+    s1.pause();
+  }};
+  
+  const p2 = document.getElementById('p2')
+  const s2 = document.getElementById("s3")
+  var count = 0;
+ function fuc2(){  
+   if (count == 0) {
+    count = 1;
+    s2.play();
+  } else {
+    count = 0;
+    s2.pause();
+  }};
+
+  const p3 = document.getElementById('p3')
+  const s3 = document.getElementById("s4")
+  var count = 0;
+ function fuc3(){  
+   if (count == 0) {
+    count = 1;
+    s3.play();
+  } else {
+    count = 0;
+    s3.pause();
+  }};
+
